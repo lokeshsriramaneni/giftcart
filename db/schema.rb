4f2dd064fd5af3d77b2673034866047fd7fd7c7c
s-avneesh <avneesh.2916@gmail.com>
2017-05-26 12:04:14 +0530

    new repo

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170526061405) do
+ActiveRecord::Schema.define(version: 20170526063043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "first_names", force: :cascade do |t|
+  
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
     t.string   "password"
