2a0facfd5899cdbf7766ba3a01ba74cb788ad320
s-avneesh <avneesh.2916@gmail.com>
2017-05-31 11:08:43 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ resources :products
 resources :carts
 resources :categories
 resources :sub_categories
-
+resources :payments do 
+  collection do
+    get "make_payments" 
+  end
+end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
